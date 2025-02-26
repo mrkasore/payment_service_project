8ddf9c99e839759d4cc9ae7157c9f9e88e4c54aa
root <root@mrkasore.fvds.ru>
2025-02-26 16:33:03 +0300

    server fix

--- stripe_project/stripe_project/settings.py
+++ stripe_project/stripe_project/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'109.172.6.59'
+]
 
 
 # Application definition
